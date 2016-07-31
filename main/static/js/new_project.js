4a92913b9fb9e9ef4c2bc7f579c9156748593f55
Özcan Esen <ozcanesen@gmail.com>
2016-07-31 03:10:18 +0300

    fix input names

--- main/static/js/new_project.js
+++ main/static/js/new_project.js
@@ -67,7 +67,6 @@ $(document).ready(function() {
     }
 
     $('form').ajaxForm({
-        type: 'post',
         beforeSend: function() {
             $('#modalUploading').modal();
             showPercent(0);
