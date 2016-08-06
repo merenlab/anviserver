8aed15cd2c706051cdab2426f7badcb1e5787be1
Özcan Esen <ozcanesen@gmail.com>
2016-08-06 16:20:06 +0300

    register all models for admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Project, ProjectUser
+from .models import Project, ProjectUser, Team, ProjectTeam, TeamUser
 
 admin.site.register(Project)
 admin.site.register(ProjectUser)
+admin.site.register(ProjectTeam)
+admin.site.register(Team)
+admin.site.register(TeamUser)
+
+
 
