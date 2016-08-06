c8579d01b388cf75fad75138f62655128bbe203a
Özcan Esen <ozcanesen@gmail.com>
2016-08-06 03:41:06 +0300

    new models

--- main/models.py
+++ main/models.py
@@ -7,13 +7,30 @@ class Project(models.Model):
     user = models.ForeignKey(User, default=1)
     name = models.CharField(max_length=100)
     desc = models.CharField(max_length=500)
+    view_key = models.CharField(max_length=16)
     created_at = models.DateTimeField(auto_now_add=True)
-
-    hasData = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username + ' - ' + self.name
 
     def get_path(self):
-    	return os.path.join(settings.USER_DATA_DIR, self.user.username, self.name)
+        return os.path.join(settings.USER_DATA_DIR, self.user.username, self.name)
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class TeamUser(models.Model):
+    team = models.ForeignKey(Project, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class ProjectUser(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    email = models.CharField(max_length=250, default="")
+    can_write = models.BooleanField(default=False)
+
+class ProjectTeam(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    can_write = models.BooleanField(default=False)
 
