b01044a16381dfec62fa93088b74abb24c7b7074
Özcan Esen <ozcanesen@gmail.com>
2016-08-08 23:23:02 +0300

    add __str__ methods for models

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 import os
 
+
 class Project(models.Model):
     user = models.ForeignKey(User, default=1)
     name = models.CharField(max_length=100)
@@ -10,30 +11,40 @@ class Project(models.Model):
     view_key = models.CharField(max_length=16)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.user.username + ' - ' + self.name
+    def __unicode__(self):
+        return 'Project ' + str(self.name) + ' (Created By: ' + str(self.user) + ')'
 
     def get_path(self):
         return os.path.join(settings.USER_DATA_DIR, self.user.username, self.name)
+
 
 class Team(models.Model):
     name = models.CharField(max_length=100)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __unicode__(self):
+        return 'Team ' + str(self.name) + ' (Leader: ' + str(self.owner) + ')'
+
+
 class TeamUser(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return str(self.user) + ' member of ' + str(self.team)
 
 class ProjectUser(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     email = models.CharField(max_length=250, default="")
     can_write = models.BooleanField(default=False)
 
-    def __str__(self):
-        return self.project.user.username + ' - ' + self.project.name + ' - ' + self.email + ' - Write:' + str(self.can_write)
+    def __unicode__(self):
+        return str(self.project) + ' shared with ' + str(self.email)
 
 class ProjectTeam(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     can_write = models.BooleanField(default=False)
 
+    def __unicode__(self):
+        return str(elf.project) + ' shared with ' + str(self.team)
