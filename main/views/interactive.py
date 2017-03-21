516d2c5deceeb1b46da8567447c1379f8a9114c4
Özcan Esen <ozcanesen@gmail.com>
2017-03-21 14:29:26 -0500

    make multiuser bar work

--- main/views/interactive.py
+++ main/views/interactive.py
@@ -8,6 +8,7 @@ from main.utils import get_project, check_view_permission, check_write_permissio
 from anvio.utils import get_names_order_from_newick_tree
 
 import zipfile
+import hashlib
 import json
 import os
 import io
@@ -56,88 +57,93 @@ def ajax_handler(request, username, project_name, view_key, requested_url):
 
     project_path = project.get_path()
 
-    try:
-        if requested_url.startswith('data/init'):
-            return JsonResponse({"title": project.name,
-                                 "description": (project.get_description()),
-                                 "clusterings": ('treeData', {'treeData': ''}),
-                                 "views": ('single', {'single': ''}),
-                                 "contigLengths": {},
-                                 "mode": "server",
-                                 "readOnly": not check_write_permission(project, request.user),
-                                 "binPrefix": "Bin_",
-                                 "sessionId": 1,
-                                 "samplesOrder": {},
-                                 "sampleInformation": {},
-                                 "sampleInformationDefaultLayerOrder": {},
-                                 "stateAutoload": None,
-                                 "collectionAutoload": None,
-                                 "noPing": True,
-                                 "inspectionAvailable": False,
-                                 "sequencesAvailable": False})
+    if requested_url.startswith('data/init'):
+        return JsonResponse({"title": project.name,
+                             "description": (project.get_description()),
+                             "clusterings": ('treeData', {'treeData': ''}),
+                             "views": ('single', {'single': ''}),
+                             "contigLengths": {},
+                             "mode": "server",
+                             "readOnly": not check_write_permission(project, request.user),
+                             "binPrefix": "Bin_",
+                             "sessionId": 1,
+                             "samplesOrder": {},
+                             "sampleInformation": {},
+                             "sampleInformationDefaultLayerOrder": {},
+                             "stateAutoload": None,
+                             "collectionAutoload": None,
+                             "noPing": True,
+                             "inspectionAvailable": False,
+                             "sequencesAvailable": False})
 
-        elif requested_url.startswith('tree/'):
-            return HttpResponse(open(os.path.join(project_path, 'treeFile')), content_type='text/plain')
+    elif requested_url.startswith('tree/'):
+        return HttpResponse(open(os.path.join(project_path, 'treeFile')), content_type='text/plain')
 
-        elif requested_url.startswith('data/view/'):
-            
-            # If data file exists convert tab separated file to json and return.
-            if os.path.exists(os.path.join(project_path, 'dataFile')):
-                data = []
-                with open(os.path.join(project_path, 'dataFile'), 'r') as f:
-                    for line in f:
-                        data.append(line.replace('\n', '').split('\t'))
+    elif requested_url.startswith('data/view/'):
+        
+        # If data file exists convert tab separated file to json and return.
+        if os.path.exists(os.path.join(project_path, 'dataFile')):
+            data = []
+            with open(os.path.join(project_path, 'dataFile'), 'r') as f:
+                for line in f:
+                    data.append(line.replace('\n', '').split('\t'))
 
-            # If data file does not exists, open newick tree generate dummy data file using leaf labels.
-            else:
-                data = [['contigs', 'names']]
-                for leaf_name in get_names_order_from_newick_tree(os.path.join(project_path, 'treeFile'), reverse=True):
-                    data.append([leaf_name, leaf_name])
+        # If data file does not exists, open newick tree generate dummy data file using leaf labels.
+        else:
+            data = [['contigs', 'names']]
+            for leaf_name in get_names_order_from_newick_tree(os.path.join(project_path, 'treeFile'), reverse=True):
+                data.append([leaf_name, leaf_name])
 
-            return JsonResponse(data, safe=False)
+        return JsonResponse(data, safe=False)
 
-        elif requested_url.startswith('data/collections'):
-            return JsonResponse(project.get_collections().collections_dict, safe=False)
+    elif requested_url.startswith('data/collections'):
+        return JsonResponse(project.get_collections().collections_dict, safe=False)
 
-        elif requested_url.startswith('data/collection/'):
-            return JsonResponse(project.get_collection(requested_url.split('/')[-1]), safe=False)
+    elif requested_url.startswith('data/collection/'):
+        return JsonResponse(project.get_collection(requested_url.split('/')[-1]), safe=False)
 
-        elif requested_url.startswith('store_collection'):
-            if not check_write_permission(project, request.user):
-                raise Http404
+    elif requested_url.startswith('store_collection'):
+        if not check_write_permission(project, request.user):
+            raise Http404
 
-            source = request.POST.get('source')
-            data = json.loads(request.POST.get('data'))
-            colors = json.loads(request.POST.get('colors'))
+        source = request.POST.get('source')
+        data = json.loads(request.POST.get('data'))
+        colors = json.loads(request.POST.get('colors'))
 
-            return JsonResponse(project.store_collection(source, data, colors), safe=False)
+        return JsonResponse(project.store_collection(source, data, colors), safe=False)
 
-        elif requested_url.startswith('store_description'):
-            if not check_write_permission(project, request.user):
-                raise Http404
+    elif requested_url.startswith('store_description'):
+        if not check_write_permission(project, request.user):
+            raise Http404
 
-            description = request.POST.get('description')
-            project.set_description(description)
+        description = request.POST.get('description')
+        project.set_description(description)
+        return JsonResponse(None, safe=False)
+
+    elif requested_url.startswith('state/all'):
+        return JsonResponse(project.get_states(), safe=False)
+
+    elif requested_url.startswith('state/get'):
+        name = request.POST.get('name')
+        states = project.get_states()
+        if name in states:
+            return JsonResponse(states[name]['content'], safe=False)
+        else:
             return JsonResponse(None, safe=False)
 
-        elif requested_url.startswith('state/all'):
-            return JsonResponse(project.get_states(), safe=False)
+    elif requested_url.startswith('state/save'):
+        if not check_write_permission(project, request.user):
+            raise Http404
 
-        elif requested_url.startswith('state/get'):
-            name = request.POST.get('name')
-            states = project.get_states()
-            if name in states:
-                return JsonResponse(states[name]['content'], safe=False)
-            else:
-                return JsonResponse(None, safe=False)
+        name = request.POST.get('name')
+        content = request.POST.get('content')
 
-        elif requested_url.startswith('state/save'):
-            if not check_write_permission(project, request.user):
-                raise Http404
+        return JsonResponse(project.store_state(name, content), safe=False)
 
-            name = request.POST.get('name')
-            content = request.POST.get('content')
-
-            return JsonResponse(project.store_state(name, content), safe=False)
-    except:
-        raise Http404
+    elif requested_url.startswith('project'):
+        content = {
+            'project_name': project.name,
+            'username': project.user.username,
+            'user_email_hash': hashlib.md5(project.user.email.encode('utf-8')).hexdigest()
+        }
+        return JsonResponse(content, safe=False)
