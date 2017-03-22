6030c5b9621f62a6f261be5f825df98975765138
Özcan Esen <ozcanesen@gmail.com>
2017-03-22 10:47:31 -0500

    if user not logged in show landing page

--- main/views/index.py
+++ main/views/index.py
@@ -2,9 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from main.models import FeedItem
 
-@login_required
 def show_index(request):
-    context = {
-        'feeditems': FeedItem.objects.filter(user=request.user).order_by('-id')[:10]
-    }
-    return render(request, 'index.html', context)
+    if request.user.is_authenticated():
+        context = {
+            'feeditems': FeedItem.objects.filter(user=request.user).order_by('-id')[:10]
+        }
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'index.html')
