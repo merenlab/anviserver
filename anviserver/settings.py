f13de96a680582112b4e9ea123a575002c9719e1
Özcan Esen <ozcanesen@gmail.com>
2017-03-30 11:41:11 -0500

    define static root

--- anviserver/settings.py
+++ anviserver/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Registration
 ACCOUNT_ACTIVATION_DAYS = 30
