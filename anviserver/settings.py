db0172dd5681026ba580f402990fd67939652081
Özcan Esen <ozcanesen@gmail.com>
2017-03-30 11:43:55 -0500

    allow every hostname

--- anviserver/settings.py
+++ anviserver/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = settings_secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
