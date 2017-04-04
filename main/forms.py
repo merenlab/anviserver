2b4bd9d3fec400e760f744c8db7981866db7d246
Özcan Esen <ozcanesen@gmail.com>
2017-04-04 13:45:41 -0500

    more strict rules for username

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,20 @@
 from registration.forms import RegistrationFormUniqueEmail
+from django.core.exceptions import ValidationError
 from django import forms
 
+import re
+
 class UserRegForm(RegistrationFormUniqueEmail):
+    username = forms.CharField(max_length=30, required=True, label='Username', help_text="Required. 30 characters or fewer. Letters, digits and _ only.")
+
     fullname = forms.CharField(max_length=100, required=False, label='Full name')
     institution = forms.CharField(max_length=100, required=False)
     orcid = forms.CharField(max_length=100, required=False, label='ORCID')
+
+    def clean_username(self):
+        username = self.cleaned_data['username'].lower()
+
+        if not re.match(r'^[a-z0-9_]*$', username):
+            raise ValidationError("") # help text already contains required information
+
+        return username
