e80af880036706e0dc42ea74d95495db67f21551
Özcan Esen <ozcanesen@gmail.com>
2017-04-23 17:03:26 -0500

    remove logged in index page

--- main/views/index.py
+++ main/views/index.py
@@ -1,12 +1,5 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-from main.models import FeedItem
 
 def show_index(request):
-    if request.user.is_authenticated():
-        context = {
-            'feeditems': FeedItem.objects.filter(user=request.user).order_by('-id')[:10]
-        }
-        return render(request, 'index.html', context)
-    else:
-        return render(request, 'index.html')
+    return render(request, 'index.html')
